e66d271676cc153fe7c41cc5991e554b95451cae
magmesser <magmesser@gmail.com>
2025-10-22 13:10:51 -0400

    Finished NavBar. Added HeroSection to render to / home page.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 // import logo from './logo.svg';
 import React from "react";
 import NavBar from "./components/NavBar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+// Pages
+import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Services from './components/pages/Services';
+// import Products from './components/pages/Products';
+// import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -10,7 +15,9 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" exact />
+          <Route index element={<Home />} />
+          {/* <Route path="/products" element={<Products />} /> */}
+          {/* <Route path='/sign-up' component={SignUp} /> */}
         </Routes>
       </Router>
     </>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,84 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "./Button";
+// import '../App.css';
+import "./NavBar.css";
+
 // Icons Import
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faXmark } from "@fortawesome/free-solid-svg-icons/faXmark";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faTypo3 } from "@fortawesome/free-brands-svg-icons";
 
 function NavBar() {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const [button, setButton] = useState(true);
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            Solstice Studios <i className="fab fa-typo3"></i>
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            Solstice Studios
+            <FontAwesomeIcon className="fa-typo3" icon={faTypo3} />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
-            <FontAwesomeIcon icon={click ? faXmark : faBars} />
+            <FontAwesomeIcon
+              className="fa-bars fa-times"
+              icon={click ? faXmark : faBars}
+            />
           </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {/* Button */}
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </>
